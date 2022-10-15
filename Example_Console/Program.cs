3d059268e30592ecae4350e36541d19a885f67a2
borodov.m <borodov.m@gmail.com>
2022-10-15 15:57:49 +0300

    Added console game

---
+++ Example_Console/Program.cs
@@ -0,0 +1,43 @@
+﻿// Консоль
+
+
+int x = 0;
+int y = 0;
+string player = "@";
+bool isPlaying = true;
+
+while(isPlaying)
+{
+    Console.Clear();
+    Console.SetCursorPosition(x,y);
+    Console.Write(player);
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    Console.CursorVisible = false;
+    if (keyInfo.Key == ConsoleKey.DownArrow)
+    {
+        y++;
+    }
+    if (keyInfo.Key == ConsoleKey.UpArrow)
+    {
+        y--;
+    }
+    if (keyInfo.Key == ConsoleKey.RightArrow)
+    {
+        x++;
+    }
+    if (keyInfo.Key == ConsoleKey.LeftArrow)
+    {
+        x--;
+    }
+    if (keyInfo.Key == ConsoleKey.E)
+    {
+        isPlaying = false;
+    }
+    //Console.WriteLine(keyInfo.Key);
+    //Console.ReadKey();
+}
+
+Console.Clear();
+Console.CursorVisible = true;
+Console.SetCursorPosition(0,0);
+Console.WriteLine("Игра окончена!");
